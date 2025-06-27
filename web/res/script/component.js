b783e93985a15e905f572e7fe404ce09ef661263
sheep-realms <sheep_realms@qq.com>
2025-06-27 21:29:12 +0800

    修复了图标搜索工具在切换显示对话框时滚动条隐现产生的页面抖动问题

--- web/res/script/component.js
+++ web/res/script/component.js
@@ -15,9 +15,32 @@ $(document).on('click', '.select-button .button:not([aria-selected="true"])', fu
     $(this).attr('aria-selected', 'true');
 });
 
+// 获取滚动条宽度
+function getScrollbarWidth() {
+    const outer = document.createElement('div');
+    outer.style.visibility = 'hidden';
+    outer.style.overflow = 'scroll'; // 强制显示滚动条
+    outer.style.position = 'absolute';
+    outer.style.top = '-9999px';
+    outer.style.width = '100px';
+
+    document.body.appendChild(outer);
+
+    const inner = document.createElement('div');
+    inner.style.width = '100%';
+    outer.appendChild(inner);
+
+    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+    outer.parentNode.removeChild(outer);
+
+    return scrollbarWidth;
+}
+
 $(document).ready(calcElementSize);
 $(window).resize(calcElementSize);
 
 function calcElementSize() {
     $('body').css('--calc-height-title-bar', $('.title-bar').height() + 'px');
+    $('body').css('--calc-scroll-bar-size', getScrollbarWidth() + 'px');
 }
